4149fd498df464c3d3b3eca8710aa238bcfba0c1
rosti <rostik.tulika@gmail.com>
2025-08-16 22:30:09 +0200

    Add environment configuration for site settings and metadata

---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  siteUrl: 'http://localhost:4200',
+  googleAnalyticsId: '',
+  pageTitle: 'Kitchen Sink',
+  pageMeta: {
+    keywords: [],
+    description: '',
+    author: ''
+  }
+};
