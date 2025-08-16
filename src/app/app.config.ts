98c8debef84c35c3be775d53ca8c30f4575244f6
rosti <rostik.tulika@gmail.com>
2025-08-16 22:24:08 +0200

    Init project configuration from elementar ui

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,64 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
-
-import { routes } from './app.routes';
-import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import {
+  ApplicationConfig, inject, PLATFORM_ID, provideAppInitializer,
+  provideBrowserGlobalErrorListeners,
+  provideZonelessChangeDetection
+} from '@angular/core';
+import {provideClientHydration, withEventReplay} from '@angular/platform-browser';
+import {provideRouter, TitleStrategy, withViewTransitions} from '@angular/router';
+import {routes} from './app.routes';
+import {COLOR_SCHEME_LOCAL_KEY, ColorScheme, ColorSchemeStore} from '@elementar-ui/components/color-scheme';
+import {provideAnimationsAsync} from '@angular/platform-browser/animations/async';
+import {provideHttpClient, withFetch} from '@angular/common/http';
+import {provideStore} from '@ngrx/store';
+import {provideNativeDateAdapter} from '@angular/material/core';
+import {ENVIRONMENT, EnvironmentService, GlobalStore, PageTitleStrategyService} from '@elementar-ui/components/core';
+import {environment} from '@environments/environment';
+import {isPlatformBrowser} from '@angular/common';
+import {MAT_FORM_FIELD_DEFAULT_OPTIONS} from '@angular/material/form-field';
+import {provideMarkdown} from 'ngx-markdown';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
-    provideRouter(routes), provideClientHydration(withEventReplay())
+    provideRouter(routes, withViewTransitions()),
+    provideClientHydration(withEventReplay()),
+    ColorSchemeStore,
+    provideAnimationsAsync(),
+    provideHttpClient(withFetch()),
+    provideStore(),
+    provideNativeDateAdapter(),
+    {
+      provide: ENVIRONMENT,
+      useValue: environment
+    },
+    provideMarkdown(),
+    provideAppInitializer(() => {
+      const envService = inject(EnvironmentService);
+      const globalStore = inject(GlobalStore);
+      const platformId = inject(PLATFORM_ID);
+      const colorSchemeStore = inject(ColorSchemeStore);
+      return new Promise((resolve, reject) => {
+        if (isPlatformBrowser(platformId)) {
+          const localColorScheme = localStorage
+            ? (localStorage.getItem(COLOR_SCHEME_LOCAL_KEY) as ColorScheme || 'light')
+            : 'light';
+          // but the best solution set it from backend
+          colorSchemeStore.setScheme(localColorScheme);
+        }
+
+        globalStore.setPageTitle(envService.getValue('pageTitle'));
+        resolve(true);
+      });
+    }),
+
+    {
+      provide: TitleStrategy,
+      useClass: PageTitleStrategyService
+    },
+    {
+      provide: MAT_FORM_FIELD_DEFAULT_OPTIONS,
+      useValue: {appearance: 'outline'}
+    }
   ]
 };
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,60 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {afterNextRender, Component, inject, OnInit} from '@angular/core';
+import {NavigationEnd, Router, RouterOutlet} from '@angular/router';
+import {SplashScreenComponent} from '@elementar-ui/components/splash-screen';
+import {TextLogoComponent} from '@elementar-ui/components/logo';
+import {PageLoadingBarComponent} from '@elementar-ui/components/page-loading-bar';
+import {
+  AnalyticsService,
+  EnvironmentService,
+  InactivityTrackerService,
+  SeoService
+} from '@elementar-ui/components/core';
+import {filter} from 'rxjs';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    TextLogoComponent,
+    SplashScreenComponent,
+    PageLoadingBarComponent
+  ],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
-export class App {
-  protected readonly title = signal('kitchensink');
+export class App implements OnInit {
+  private _analyticsService = inject(AnalyticsService);
+  private _inactivityTracker = inject(InactivityTrackerService);
+  private _seoService = inject(SeoService);
+  private _envService = inject(EnvironmentService);
+  private _router = inject(Router);
+
+  constructor() {
+    afterNextRender(() => {
+      // Scroll a page to top if url changed
+      this._router.events
+        .pipe(
+          filter(event => event instanceof NavigationEnd)
+        )
+        .subscribe(() => {
+          window.scrollTo({
+            top: 0,
+            left: 0
+          });
+        })
+      ;
+
+      this._analyticsService.trackPageViews();
+      this._inactivityTracker.setupInactivityTimer()
+        .subscribe(() => {
+          // console.log('Inactive mode has been activated!');
+          // this._inactivityTracker.reset();
+        })
+      ;
+    });
+  }
+
+  ngOnInit(): void {
+    this._seoService.trackCanonicalChanges(this._envService.getValue('siteUrl'));
+  }
 }
