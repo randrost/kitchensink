4123f207e9e007a9a727e16ab6a4c095b1053899
rosti <rostik.tulika@gmail.com>
2025-08-17 16:59:47 +0200

    Add routing configuration with error handling and main component paths

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,37 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
 
 export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'error',
+    loadChildren: () => import('@features/error/error.module').then(m => m.ErrorModule)
+  },
+  {
+    path: '',
+    loadComponent: () => import('@shared/component/common/common.component').then(c => c.CommonComponent),
+    children: [
+      {
+        path: '',
+        pathMatch: 'full',
+        loadComponent: () => import('@features/main/main.component').then(c => c.MainComponent),
+        title: 'Introduction',
+        data: {
+          icon: 'home',
+        }
+      },
+      {
+        path: 'widget-grid',
+        loadComponent: () => import('@features/widget-grid/widget-grid').then(c => c.WidgetGrid),
+        title: 'Widget Grid',
+        data: {
+          icon: 'dashboard',
+        }
+      },
+    ]
+  },
+  {
+    path: '**',
+    title: 'Page Not Found',
+    loadComponent: () => import('@features/error/not-found/not-found.component').then(c => c.NotFoundComponent)
+  }
+];
